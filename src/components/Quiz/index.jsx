82ceef27c37972425988f11a95891fee95ba9c87
Denisa Zemanova <xdenisa.zemanova@gmail.com>
2024-05-23 14:05:04 +0200

    Přidání komponent Quiz a Sound

---
+++ src/components/Quiz/index.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import "./style.css"
+
+const questions = [
+  // 0
+  {
+    questionText: 'Jakou máš náladičku?',
+    answerOptions:  [
+      { answerText: 'Skvělou', nextQuestionIndex: 1 },
+      { answerText: 'Dobrou', nextQuestionIndex: 4 },
+      { answerText: 'Ujde to', nextQuestionIndex: 4 },
+      { answerText: 'Špatnou', nextQuestionIndex: 3 },
+      { answerText: 'Velmi špatnou', nextQuestionIndex: 3 },
+    ],
+  },
+  // 1
+  {
+    questionText: 'Proč ji máš skvělou?',
+    answerOptions: [
+      { answerText: 'Proto', nextQuestionIndex: 4 },
+      { answerText: 'Nevim', nextQuestionIndex: 4 },
+      { answerText: 'Jentak', nextQuestionIndex: 4 },
+      { answerText: 'Nečum', nextQuestionIndex: 4 },
+      { answerText: 'Svítí slunce', nextQuestionIndex: 4 },
+    ],
+  },
+  // 2
+  {
+    questionText: 'Nevíš, jak je to možné?',
+    answerOptions: [
+      { answerText: 'Proto', nextQuestionIndex: 4 },
+      { answerText: 'Nevim', nextQuestionIndex: 4 },
+      { answerText: 'Jentak', nextQuestionIndex: 4 },
+      { answerText: 'Nečum', nextQuestionIndex: 4 },
+      { answerText: 'Svítí slunce', nextQuestionIndex: 4 },
+    ],
+  },
+  // 3
+  {
+    questionText: 'Máš často špatnou náladu?',
+    answerOptions: [
+      { answerText: 'Nikdy', nextQuestionIndex: 4 },
+      { answerText: 'Občas', nextQuestionIndex: 4 },
+      { answerText: 'Často', nextQuestionIndex: 4 },
+      { answerText: 'Velmi často', nextQuestionIndex: 4 },
+      { answerText: 'Vždy', nextQuestionIndex: 4 },
+    ],
+  },
+  // 4
+  {
+    questionText: 'Jak často se snažíte být přítomní v daném okamžiku?',
+    answerOptions: [
+      { answerText: 'Nikdy', nextQuestionIndex: null },
+      { answerText: 'Občas', nextQuestionIndex: null },
+      { answerText: 'Často', nextQuestionIndex: null },
+      { answerText: 'Velmi často', nextQuestionIndex: null },
+      { answerText: 'Vždy', nextQuestionIndex: null },
+    ],
+  },
+];
+
+const Quiz = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  // const [lastSentence, setLastSentence] = useState(false);
+
+  const handleAnswerOptionClick = (nextQuestionIndex) => {
+    setCurrentQuestion(nextQuestionIndex);
+  };
+
+  return (
+    <div className='container'>
+      <h1>Dotazník</h1>
+      {currentQuestion === null ? (
+        <div className='lastSentence-section'>
+          Dokončil jsi dotazníček.
+        </div>
+      ) : (
+        <>
+          <div className='question-section'>
+            <div className='question-count'>
+              {/* Odpočítávadlo otázek: */}
+              <span>Otázka {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className='question-text'>{questions[currentQuestion].questionText}</div>
+          </div>
+          <div className='answer-section'>
+            {questions[currentQuestion].answerOptions.map((answerOption, index) => (
+              <button key={index} onClick={() => handleAnswerOptionClick(answerOption.nextQuestionIndex)}>
+                {answerOption.answerText}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Quiz;
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,10 +1,14 @@
 import { Navbar } from "../../components/Navbar";
+import Quiz from "../../components/Quiz";
+import Sounds from "../../components/Sound";
 import "./style.css";
 
 export const HomePage = () => {
   return (
     <div className="container">
       <Navbar />
+      <Quiz />
+      {/* <Sounds /> */}
     </div>
   );
 };
