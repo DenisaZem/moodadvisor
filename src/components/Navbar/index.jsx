9c5e4c3a16d9313cff3dcab5676708e48193ffa6
Šárka Flídrová <sarka.flidrova@email.cz>
2024-05-23 15:11:36 +0200

    Navbar přidán do samostatné komponenty

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,46 +1,44 @@
 import "./style.css";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-    const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
-    const changeLanguage = (lng) => {
-      i18n.changeLanguage(lng);
-    };
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
 
-    return (
-        <nav>
-        <div className="nav-bar">
-          <h1 className="logo">{t("logo")}</h1>
-          <ul className="menu">
-            <li>
-              <a className="menu--item" href="./">
-                {t("navigation.home")}
-              </a>
-            </li>
-            <li>
-              <a className="menu--item" href="">
-                {t("navigation.breath")}
-              </a>
-            </li>
-            <li>
-              <a className="menu--item" href="">
-                {t("navigation.music")}
-              </a>
-            </li>
-            <li>
-              <button type="button" onClick={() => changeLanguage("cs")}>
-                cs
-              </button>
-            </li>
-            <li>
-              {" "}
-              <button type="button" onClick={() => changeLanguage("en")}>
-                en
-              </button>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    )
-}
+  return (
+    <nav>
+      <div className="nav-bar">
+        <h1 className="logo">{t("logo")}</h1>
+        <ul className="menu">
+          <li>
+            <Link to="/">{t("navigation.home")}</Link>
+          </li>
+          <li>
+            <Link to="/breath">{t("navigation.breath")}</Link>
+          </li>
+          <li>
+            <Link to="/music">{t("navigation.music")}</Link>
+          </li>
+          <li>
+            <Link to="/quiz">{t("navigation.quiz")}</Link>
+          </li>
+          <li>
+            <button type="button" onClick={() => changeLanguage("cs")}>
+              cs
+            </button>
+          </li>
+          <li>
+            {" "}
+            <button type="button" onClick={() => changeLanguage("en")}>
+              en
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -13,47 +13,14 @@ import { HomePage } from "./pages/HomePage";
 import Sound from "./components/Sound";
 import Quiz from "./components/Quiz";
 import { ErrorPage } from "./pages/ErrorPage";
+import { Navbar } from "./components/Navbar";
 
 // import npm install react-router-dom
 
 export const App = () => {
-  const { t, i18n } = useTranslation();
-
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
-  };
   return (
     <Suspense fallback="loading">
-      <nav>
-        <div className="nav-bar">
-          <h1 className="logo">{t("logo")}</h1>
-          <ul className="menu">
-            <li>
-              <Link to="/">{t("navigation.home")}</Link>
-            </li>
-            <li>
-              <Link to="/breath">{t("navigation.breath")}</Link>
-            </li>
-            <li>
-              <Link to="/music">{t("navigation.music")}</Link>
-            </li>
-            <li>
-              <Link to="/quiz">{t("navigation.quiz")}</Link>
-            </li>
-            <li>
-              <button type="button" onClick={() => changeLanguage("cs")}>
-                cs
-              </button>
-            </li>
-            <li>
-              {" "}
-              <button type="button" onClick={() => changeLanguage("en")}>
-                en
-              </button>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar />
       {/* <HomePage /> */}
       <Outlet />
     </Suspense>
