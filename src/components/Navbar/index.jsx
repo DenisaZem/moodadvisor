b65e298e2e10f3dc6d9f4a8e320a8744c2818769
Šárka Flídrová <sarka.flidrova@email.cz>
2024-05-24 14:00:20 +0200

    Navbar obnoven do původního stavu.

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
 import "./style.css";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
-import Hamburger from "hamburger-react";
-
-// npm install hamburger-react https://hamburger-react.netlify.app/
 
 export const Navbar = () => {
-  const [isOpen, setOpen] = useState(false);
   const { t, i18n } = useTranslation();
 
   const changeLanguage = (lng) => {
@@ -16,38 +11,6 @@ export const Navbar = () => {
 
   return (
     <nav>
-      <div className="nav-bar">
-        <h1 className="logo">{t("logo")}</h1>
-        <Hamburger toggled={isOpen} toggle={setOpen} duration={0.8} color="#4a6670"/>
-        {isOpen && (
-            <ul className="menu__mobile">
-              <li>
-                <Link to="/">{t("navigation.home")}</Link>
-              </li>
-              <li>
-                <Link to="/breath">{t("navigation.breath")}</Link>
-              </li>
-              <li>
-                <Link to="/music">{t("navigation.music")}</Link>
-              </li>
-              <li>
-                <Link to="/quiz">{t("navigation.quiz")}</Link>
-              </li>
-              <li>
-                <button type="button" onClick={() => changeLanguage("cs")}>
-                  cs
-                </button>
-              </li>
-              <li>
-                {" "}
-                <button type="button" onClick={() => changeLanguage("en")}>
-                  en
-                </button>
-              </li>
-            </ul>
-        )}
-      </div>
-
       <div className="nav-bar">
         <h1 className="logo">{t("logo")}</h1>
         <ul className="menu">
