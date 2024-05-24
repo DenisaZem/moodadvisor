8f771235d4f37cec36d9e87a9e4e28a236693b1d
Šárka Flídrová <sarka.flidrova@email.cz>
2024-05-24 16:59:14 +0200

    Upravena struktura komp Nabar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -10,35 +10,40 @@ export const Navbar = () => {
   };
 
   return (
-    <nav>
-      <div className="nav-bar">
-        <h1 className="logo">{t("logo")}</h1>
-        <ul className="menu">
-          <li>
-            <Link to="/">{t("navigation.home")}</Link>
-          </li>
-          <li>
-            <Link to="/breath">{t("navigation.breath")}</Link>
-          </li>
-          <li>
-            <Link to="/music">{t("navigation.music")}</Link>
-          </li>
-          <li>
-            <Link to="/quiz">{t("navigation.quiz")}</Link>
-          </li>
-          <li>
-            <button type="button" onClick={() => changeLanguage("cs")}>
-              cs
-            </button>
-          </li>
-          <li>
-            {" "}
-            <button type="button" onClick={() => changeLanguage("en")}>
-              en
-            </button>
-          </li>
-        </ul>
-      </div>
+    <nav className="nav-bar">
+      <h1 className="logo">{t("logo")}</h1>
+      <ul className="menu">
+        <li>
+          <Link className="menu--item" to="/">
+            {t("navigation.home")}
+          </Link>
+        </li>
+        <li>
+          <Link className="menu--item" to="/breath">
+            {t("navigation.breath")}
+          </Link>
+        </li>
+        <li>
+          <Link className="menu--item" to="/music">
+            {t("navigation.music")}
+          </Link>
+        </li>
+        <li>
+          <Link className="menu--item" to="/quiz">
+            {t("navigation.quiz")}
+          </Link>
+        </li>
+        <li>
+          <button type="button" onClick={() => changeLanguage("cs")}>
+            cs
+          </button>
+        </li>
+        <li>
+          <button type="button" onClick={() => changeLanguage("en")}>
+            en
+          </button>
+        </li>
+      </ul>
     </nav>
   );
 };
