e8457241748e781400c5873a6bb82e4c3199eed8
Denisa Zemanova <xdenisa.zemanova@gmail.com>
2024-05-27 13:45:06 +0200

    com. SoundMenu-přidání Swiper a EffectCoverflow modulu

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,12 +1,10 @@
 import Quiz from "../../components/Quiz";
-import { SoundMenu } from "../SoundMenu";
 import "./style.css";
 
 export const HomePage = () => {
   return (
     <>
       <Quiz />
-      <SoundMenu />
     </>
   );
 };
