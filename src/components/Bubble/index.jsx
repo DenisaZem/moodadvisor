8585f2cbf17ae05fe8b8873d28db713c0e29d926
DenisaZem <xdenisa.zemanova@gmail.com>
2024-05-31 15:07:16 +0200

    Draft dýchací bubliny

--- src/components/Bubble/index.jsx
+++ src/components/Bubble/index.jsx
@@ -4,22 +4,33 @@ import { motion } from "framer-motion";
 
 const Bubble = () => {
   return (
-        <motion.div
-          className="box"
-          animate={{
-            scale: [1, 2, 2, 1, 1],
-            rotate: [0, 0, 180, 180, 0],
-            borderRadius: ["0%", "0%", "50%", "50%", "0%"]
-          }}
-          transition={{
-            duration: 2,
-            ease: "easeInOut",
-            times: [0, 0.2, 0.5, 0.8, 1],
-            repeat: Infinity,
-            repeatDelay: 1
-          }}
-        />
-      );
+    <motion.div
+      className="bubble"
+      animate={{
+        scale: [1, 1.1, 1.1, 0.95, 1], 
+      }}
+      transition={{
+        duration: 4,
+        ease: "easeInOut",
+        repeat: Infinity,
+        repeatDelay: 1,
+      }}
+      style={{
+        width: 250,
+        height: 250,
+        background: "blue",
+        borderRadius: "50%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white",
+        fontSize: "20px",
+        fontWeight: "bold",
+      }}
+    >
+      Zhluboka se nadechni
+    </motion.div>
+  ); 
 }
 
 export default Bubble
