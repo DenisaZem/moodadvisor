18de862c057c75e0815e26e19431831e01ef0291
Šárka Flídrová <sarka.flidrova@email.cz>
2024-05-31 10:47:55 +0200

    com. Navbar: přidáno jednotné tlačítko na přepínání jazyka

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,15 +1,27 @@
-import './style.css';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { IoClose } from 'react-icons/io5';
-import React, { useState } from 'react';
+import "./style.css";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
+import React, { useState } from "react";
 
 export const Navbar = () => {
   const { t, i18n } = useTranslation();
+  const currentLanguage = i18n.language;
 
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
+  // const changeLanguage = (lng) => {
+  //   i18n.changeLanguage(lng);
+  // };
+
+  const toggleLanguage = () => {
+    if (currentLanguage === "cs") {
+      i18n.changeLanguage("en");
+    } else if (currentLanguage === "en") {
+      i18n.changeLanguage("cs");
+    }
+    // else {
+    //   i18n.changeLanguage("en")
+    // }
   };
 
   const [showMenu, setShowMenu] = useState(false);
@@ -21,51 +33,74 @@ export const Navbar = () => {
   return (
     <div>
       <div
-        className={showMenu ? 'background_black open' : 'background_black'}
+        className={showMenu ? "background_black open" : "background_black"}
       ></div>
-      <div className={showMenu ? 'background open' : 'background'}></div>
-    <nav className="nav-bar">
-      <h1 className="logo"><Link to="/">{t('logo')}</Link></h1>
-      {showMenu ? (
-        <IoClose className="menu-icon-cancel" onClick={toggleMenu} />
-      ) : (
-        <GiHamburgerMenu className="menu-icon" onClick={toggleMenu} />
-      )}
-      <ul className={`menu ${showMenu ? 'show' : ''}`}>
-        <li>
-          <Link onClick={showMenu ? toggleMenu : null} className="menu--item" to="/">
-            {t('navigation.home')}
-          </Link>
-        </li>
-        <li>
-          <Link onClick={showMenu ? toggleMenu : null} className="menu--item" to="/breath">
-            {t('navigation.breath')}
-          </Link>
-        </li>
-        <li>
-          <Link onClick={showMenu ? toggleMenu : null} className="menu--item" to="/music">
-            {t('navigation.music')}
-          </Link>
-        </li>
-        <li>
-          <Link onClick={showMenu ? toggleMenu : null} className="menu--item" to="/quiz">
-            {t('navigation.quiz')}
-          </Link>
-        </li>
-        <div className="buttons-language">
-          <li className="button-language__cs">
-            <button type="button" onClick={() => changeLanguage('cs')}>
-              cs
-            </button>
+      <div className={showMenu ? "background open" : "background"}></div>
+      <nav className="nav-bar">
+        <h1 className="logo">
+          <Link to="/">{t("logo")}</Link>
+        </h1>
+        {showMenu ? (
+          <IoClose className="menu-icon-cancel" onClick={toggleMenu} />
+        ) : (
+          <GiHamburgerMenu className="menu-icon" onClick={toggleMenu} />
+        )}
+        <ul className={`menu ${showMenu ? "show" : ""}`}>
+          <li>
+            <Link
+              onClick={showMenu ? toggleMenu : null}
+              className="menu--item"
+              to="/"
+            >
+              {t("navigation.home")}
+            </Link>
           </li>
-          <li className="button-language__en">
-            <button type="button" onClick={() => changeLanguage('en')}>
-              en
-            </button>
+          <li>
+            <Link
+              onClick={showMenu ? toggleMenu : null}
+              className="menu--item"
+              to="/breath"
+            >
+              {t("navigation.breath")}
+            </Link>
           </li>
-        </div>
-      </ul>
-    </nav>
+          <li>
+            <Link
+              onClick={showMenu ? toggleMenu : null}
+              className="menu--item"
+              to="/music"
+            >
+              {t("navigation.music")}
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={showMenu ? toggleMenu : null}
+              className="menu--item"
+              to="/quiz"
+            >
+              {t("navigation.quiz")}
+            </Link>
+          </li>
+          <div className="buttons-language">
+            {/* <li className="button-language__cs">
+              <button type="button" onClick={() => changeLanguage("cs")}>
+                cs
+              </button>
+            </li>
+            <li className="button-language__en">
+              <button type="button" onClick={() => changeLanguage("en")}>
+                en
+              </button>
+            </li> */}
+            <li>
+              <button className="button--toggle-language" onClick={toggleLanguage}>
+                {currentLanguage === "en" ? "Česky" : "English"}
+              </button>
+            </li>
+          </div>
+        </ul>
+      </nav>
     </div>
   );
 };
