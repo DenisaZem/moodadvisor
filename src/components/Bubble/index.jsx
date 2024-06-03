a0b5a187b04d503d1d393ab32a39a2a05b89524e
DenisaZem <xdenisa.zemanova@gmail.com>
2024-06-03 10:20:20 +0200

    com. Bubble: přidáno pozadí

--- src/components/Bubble/index.jsx
+++ src/components/Bubble/index.jsx
@@ -4,32 +4,33 @@ import { motion } from "framer-motion";
 
 const Bubble = () => {
   return (
-    <motion.div
-      className="bubble"
-      animate={{
-        scale: [1, 1.1, 1.1, 0.95, 1], 
-      }}
-      transition={{
-        duration: 4,
-        ease: "easeInOut",
-        repeat: Infinity,
-        repeatDelay: 1,
-      }}
-      style={{
-        width: 250,
-        height: 250,
-        background: "blue",
-        borderRadius: "50%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        color: "white",
-        fontSize: "20px",
-        fontWeight: "bold",
-      }}
-    >
-      Zhluboka se nadechni
-    </motion.div>
+    <div className='background'>
+      <motion.div
+        className="bubble"
+        animate={{
+          scale: [1, 1.1, 1.1, 0.95, 1], 
+        }}
+        transition={{ 
+          duration: 4,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatDelay: 1,
+        }}
+        style={{
+          width: 250,
+          height: 250,
+          borderRadius: "50%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "white",
+          fontSize: "20px",
+          fontWeight: "bold",
+        }}
+      >
+        Zhluboka se nadechni
+      </motion.div>
+    </div>
   ); 
 }
 
