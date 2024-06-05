1abe34328a28d219d37aabf10a6e2ac010481f9d
DenisaZem <xdenisa.zemanova@gmail.com>
2024-06-05 13:21:48 +0200

    com. BreathBubble: Nastavení zjevení bubliny_nefunkční

--- src/components/BreathBubble/index.jsx
+++ src/components/BreathBubble/index.jsx
@@ -1,14 +1,43 @@
 import "./style.css";
-
-import React from 'react'
+import { motion } from "framer-motion";
+import React, { useState } from "react";
 
 export const BreathBubble = () => {
+  const [showBubble, setShowBubble] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowBubble(true);
+  };
+
   return (
     <div>
-
-        bubble zkouška
-      
+      <motion.button
+        onClick={handleButtonClick}
+        className="bub"
+        animate={{
+          scale: [1, 1.25, 1.3, 1.1, 1.2, 1.3, 1.4, 1.5, 1.3, 1.4],
+        }}
+        transition={{
+          duration: 6,
+          ease: "linear",
+          repeat: Infinity,
+          repeatDelay: 0,
+        }}
+        style={{
+          width: 200,
+          height: 200,
+          borderRadius: "50%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "white",
+          fontSize: "20px",
+          fontWeight: "bold",
+          textAlign: "center",
+        }}
+      >
+        Ahoj
+      </motion.button>
     </div>
-  )
-}
-
+  );
+};
