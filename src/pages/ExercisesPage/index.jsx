e1bba4565decfcac956d71ad19a836af028ea791
Šárka Flídrová <sarka.flidrova@email.cz>
2024-06-07 11:13:14 +0200

    com. ExercisesPage: upraveno rozložení dlaždic

--- src/pages/ExercisesPage/index.jsx
+++ src/pages/ExercisesPage/index.jsx
@@ -30,23 +30,45 @@ export const ExercisesPage = () => {
     <div className="container">
       <h1 className="exercise-title">Cvičení</h1>
       <div className="exercise-menu">
-        {exercisesData.map((item) => {
-          return (
             <Link
-              to={`/exercises/${item.url}`}
-              key={item.id}
+              to={`/exercises/${exercisesData[0].url}`}
               className="exercise-menu--tile"
               href=""
             >
               <img
-                src={item.picture}
+                src={exercisesData[0].picture}
                 className="container__exerciseLink"
                 alt="picture"
               />
-              <div className="container__exerciseLink">{item.title}</div>
+              <div className="container__exerciseLink">{exercisesData[0].title}</div>
             </Link>
-          );
-        })}
+
+            <Link
+              to={`/exercises/${exercisesData[1].url}`}
+              className="exercise-menu--tile"
+              href=""
+            >
+              <div className="container__exerciseLink">{exercisesData[1].title}</div>
+              <img
+                src={exercisesData[1].picture}
+                className="container__exerciseLink"
+                alt="picture"
+              />
+            </Link>
+
+            <Link
+              to={`/exercises/${exercisesData[2].url}`}
+              className="exercise-menu--tile"
+              href=""
+            >
+              <img
+                src={exercisesData[2].picture}
+                className="container__exerciseLink"
+                alt="picture"
+              />
+              <div className="container__exerciseLink">{exercisesData[2].title}</div>
+            </Link>
+
       </div>
     </div>
   );
