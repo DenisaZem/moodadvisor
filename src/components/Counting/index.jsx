f3f335b056fec4e0f9b9df7f88432dd487490671
Šárka Flídrová <sarka.flidrova@email.cz>
2024-06-07 15:51:24 +0200

    com. Counting: přidán formulář s počty

--- src/components/Counting/index.jsx
+++ src/components/Counting/index.jsx
@@ -1,9 +1,49 @@
-import "./style.css"
+import { useEffect, useState } from "react";
+import "./style.css";
 
 export const Counting = () => {
-    return (
-        <div className="container">
-            Zde bude cvičení s počítáním.
-        </div>
-    )
-}
+  const [answer, setAnswer] = useState(0);
+  const [numbers, setNumbers] = useState([]);
+
+  const handleChange = (event) => {
+    setAnswer(event.target.value);
+  };
+
+  const newNumbers = () => {
+    const number1 = Math.floor(Math.random() * 50);
+    const number2 = Math.floor(Math.random() * 50);
+    setNumbers([number1, number2]);
+  };
+
+  useEffect(() => newNumbers(), []);
+
+  const handleAnswer = () => {
+    const total = numbers[0] + numbers[1];
+    if (total === Number(answer)) {
+        console.log("dobře!")
+      return <div>Dobrá práce!</div>;
+    } else {
+        console.log("špatně!")
+      return <div>Zkus to znovu</div>;
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Počítání</h1>
+      <p>Zaměřte se na tato jednoduchá matematická cvičení.</p>
+      <form action="">
+        <label htmlFor="">
+          {numbers[0]} + {numbers[1]} =
+        </label>
+        <input
+          onChange={handleChange}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") handleAnswer();
+          }}
+          type="number"
+        />
+      </form>
+    </div>
+  );
+};
