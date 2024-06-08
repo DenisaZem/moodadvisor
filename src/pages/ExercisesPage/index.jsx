1d931f51d75110bd1bec4aec5dfa20ea389b06f7
Šárka Flídrová <sarka.flidrova@email.cz>
2024-06-08 17:42:21 +0200

    com. ExercisesPages: zeštíhlena

--- src/pages/ExercisesPage/index.jsx
+++ src/pages/ExercisesPage/index.jsx
@@ -8,19 +8,16 @@ import { useTranslation } from "react-i18next";
 export const exercisesData = [
   // 0
   {
-    title: "exercises.0",
     url: "senses",
     picture: obrazek1,
   },
   // 1
   {
-    title: "exercises.1",
     url: "counting",
     picture: obrazek2,
   },
   // 2
   {
-    title: "exercises.2",
     url: "map",
     picture: obrazek3,
   },
@@ -32,45 +29,23 @@ export const ExercisesPage = () => {
     <div className="container">
       <h1 className="exercise-title">Cvičení</h1>
       <div className="exercise-menu">
+        {exercisesData.map((item, index) => {
+          return (
             <Link
-              to={`/exercises/${exercisesData[0].url}`}
+              key={index}
+              to={`/exercises/${item.url}`}
               className="exercise-menu--tile"
               href=""
             >
               <img
-                src={exercisesData[0].picture}
+                src={item.picture}
                 className="container__exerciseLink"
                 alt="picture"
               />
-              <div className="container__exerciseLink">{t(exercisesData[0].title)}</div>
+              <div className="container__exerciseLink">{t(`exercises.${index}`)}</div>
             </Link>
-
-            <Link
-              to={`/exercises/${exercisesData[1].url}`}
-              className="exercise-menu--tile"
-              href=""
-            >
-              <div className="container__exerciseLink">{t(exercisesData[1].title)}</div>
-              <img
-                src={exercisesData[1].picture}
-                className="container__exerciseLink"
-                alt="picture"
-              />
-            </Link>
-
-            <Link
-              to={`/exercises/${exercisesData[2].url}`}
-              className="exercise-menu--tile"
-              href=""
-            >
-              <img
-                src={exercisesData[2].picture}
-                className="container__exerciseLink"
-                alt="picture"
-              />
-              <div className="container__exerciseLink">{t(exercisesData[2].title)}</div>
-            </Link>
-
+          )
+        })}
       </div>
     </div>
   );
