1d9f47a2e675a33052978d6d660aaf802fdccd7a
DenisaZem <xdenisa.zemanova@gmail.com>
2024-06-09 18:55:24 +0200

    com. Quiz: ňú kontejner

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -221,61 +221,63 @@ const Quiz = () => {
   };
 
   return (
-    <div className="container__mainQuiz">
-      {showBubble && <Bubble handleClick={handleClick} />}
-      {/* <h1 className="container__mainQuiz--title">Mood Quiz</h1> */}
-      <div className="container__quiz">
-        {currentQuestion === null ? (
-          <div className="lastSentence-section">
-            {/* <div className="lastSentence-section__title">
+    <div className="quiz">
+      <div className="quiz__container">
+        {showBubble && <Bubble handleClick={handleClick} />}
+        {/* <h1 className="container__mainQuiz--title">Mood Quiz</h1> */}
+        <div className="container__quiz">
+          {currentQuestion === null ? (
+            <div className="lastSentence-section">
+              {/* <div className="lastSentence-section__title">
               Dokončil jsi dotazníček.
             </div> */}
-            <div className="lastSentence-section__recommended">
-              {t("quiz.last")}
-            </div>
-            <div className="lastSentence-section__menu">
-              <div className="lastSentence-section__menu--items">
-                <a href="/breath">
-                  <img src={breath} alt="breath" />
-                </a>
-                <a href="/music">
-                  <img src={music} alt="music" />
-                </a>
-                <a href="/exercises">
-                  <img src={music} alt="exercises" />
-                </a>
+              <div className="lastSentence-section__recommended">
+                {t("quiz.last")}
+              </div>
+              <div className="lastSentence-section__menu">
+                <div className="lastSentence-section__menu--items">
+                  <a href="/breath">
+                    <img src={breath} alt="breath" />
+                  </a>
+                  <a href="/music">
+                    <img src={music} alt="music" />
+                  </a>
+                  <a href="/exercises">
+                    <img src={music} alt="exercises" />
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        ) : (
-          <>
-            <div className="question__section">
-              <div className="question-text">
-                {t(`quiz.questions.${currentQuestion}.questionText`)}
+          ) : (
+            <>
+              <div className="question__section">
+                <div className="question-text">
+                  {t(`quiz.questions.${currentQuestion}.questionText`)}
+                </div>
               </div>
-            </div>
-            <div className="answer__section">
-              {questions[currentQuestion].answerOptions.map(
-                (answerOption, index) => (
-                  <button
-                    key={index}
-                    className="answer__section--button"
-                    onClick={() =>
-                      handleAnswerOptionClick(
-                        answerOption.nextQuestionIndex,
-                        t(answerOption.link)
-                      )
-                    }
-                  >
-                    {t(
-                      `quiz.questions.${currentQuestion}.answerOptions.${index}.answer`
-                    )}
-                  </button>
-                )
-              )}
-            </div>
-          </>
-        )}
+              <div className="answer__section">
+                {questions[currentQuestion].answerOptions.map(
+                  (answerOption, index) => (
+                    <button
+                      key={index}
+                      className="answer__section--button"
+                      onClick={() =>
+                        handleAnswerOptionClick(
+                          answerOption.nextQuestionIndex,
+                          t(answerOption.link)
+                        )
+                      }
+                    >
+                      {t(
+                        `quiz.questions.${currentQuestion}.answerOptions.${index}.answer`
+                      )}
+                    </button>
+                  )
+                )}
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
