f2af7f5f01a5432a9cb186fa85dc8a4ba35affa8
Šárka Flídrová <sarka.flidrova@email.cz>
2024-06-11 12:10:00 +0200

    com. Footer: zmenšen footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,20 +1,23 @@
-import { Link } from "react-router-dom"
-import "./style.css"
-import { useTranslation } from "react-i18next"
+import { Link } from "react-router-dom";
+import "./style.css";
+import { useTranslation } from "react-i18next";
 
 export const Footer = () => {
-    const {t} = useTranslation(); 
+  const { t } = useTranslation();
 
-    return ( 
-        <footer className="footer">
-            <ul className="footer--menu">
-                <li className="footer--menu--item"><Link to="/about-us">{t("footer.aboutUs")}</Link></li>
-                <li className="footer--menu--item"><Link to="/about">{t("footer.aboutProject")}</Link></li>
-                <li className="footer--menu--item"><a href="https://www.czechitas.cz/">{t("footer.organization")}</a></li>
-            </ul>
-            <p className="footer--copy">
-            {t("footer.copy")}
-            </p>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer">
+      <ul className="footer--menu">
+        <li className="footer--menu--item">
+          <Link to="/about-us">{t("footer.aboutUs")}</Link>
+        </li>
+        <li className="footer--menu--item">
+          <Link to="/about">{t("footer.aboutProject")}</Link>
+        </li>
+        <li className="footer--menu--item">
+          <a href="https://www.czechitas.cz/">{t("footer.organization")}</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
