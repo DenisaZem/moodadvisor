fdd63e43e298fd5e85d10ebef6010b2a2234fec1
Šárka Flídrová <sarka.flidrova@email.cz>
2024-06-12 12:46:18 +0200

    com. AboutPage: upraveno pro mobil

--- src/pages/AboutPage/index.jsx
+++ src/pages/AboutPage/index.jsx
@@ -1,34 +1,18 @@
 import "./style.css";
-import zarovka from "./img/zarovka.jpg"
-import note from "./img/note.jpg"
+import zarovka from "./img/zarovka.jpg";
+import note from "./img/note.jpg";
 import { useTranslation } from "react-i18next";
 
 export const AboutPage = () => {
-    const {t} = useTranslation();
+  const { t } = useTranslation();
 
   return (
-    <div className="container">
-      <div className="AboutProject">
-        <div className="AboutProject__title">
-          <div className="about-project__container1">
-            <img
-              className="about__project--zarovkaImg"
-              src={zarovka}
-              alt="zarovka"
-            />
-            <h2 className="about-project--name">{t("about.title")}</h2>
-          </div>
-          <div className="about-project__container2">
-            <p className="about__project--title">
-                {t("about.description")}
-            </p>
-            <img
-              className="about__project--noteImg"
-              src={note}
-              alt="blocek"
-            />
-          </div>
-        </div>
+    <div className="about-project--container">
+      <h2 className="about-project--logo">{t("about.title")}</h2>
+      <p className="about-project--title">{t("about.description")}</p>
+      <div className="about-project--imgs">
+        <img className="about-project--img" src={zarovka} alt="zarovka" />
+        <img className="about-project--img" src={note} alt="blocek" />
       </div>
     </div>
   );
