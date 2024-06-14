8bd8a928a5a7529686f497d72e5838fed5cd2d41
Šárka Flídrová <sarka.flidrova@email.cz>
2024-06-14 19:23:00 +0200

    com. AboutUsPage: upraveny základní styly na nový vzhled, vyměněny obrázky

--- src/pages/AboutUsPage/index.jsx
+++ src/pages/AboutUsPage/index.jsx
@@ -7,40 +7,56 @@ export const AboutUsPage = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="container">
-      <div className="about-us">
-        <div className="about-us--title">
-          <h2 className="about-us--title--name">{t("aboutUs.denisa.title")}</h2>
-          <span>
-            <a href="">LinkedIn</a> | <a href="">GitHub</a>
-          </span>
-          <h3 className="about-us--title--name__deni">
-            {t("aboutUs.denisa.subtitle")}
-          </h3>
-          <p className="about-us--title--paragraph">
-            {t("aboutUs.denisa.description")}
-          </p>
-          <img className="about-us--title--img" src={deni} alt="deni avatar" />
-        </div>
+    <div className="container-about-us">
+      <div className="about-us--title">
+        <h2 className="about-us--item about-us--title--name">
+          {t("aboutUs.denisa.title")}
+        </h2>
+        <span className="about-us--item ">
+          <a className="about-us--link" href="">
+            LinkedIn
+          </a>{" "}
+          |{" "}
+          <a className="about-us--link" href="">
+            GitHub
+          </a>
+        </span>
+        <h3 className="about-us--item about-us--title--name__deni">
+          {t("aboutUs.denisa.subtitle")}
+        </h3>
+        <p className="about-us--item about-us--title--paragraph">
+          {t("aboutUs.denisa.description")}
+        </p>
+        <img className="about-us--title--img" src={deni} alt="deni avatar" />
+      </div>
 
-        <div className="about-us--title about-us--title--sarka">
-          <h2 className="about-us--title--name">{t("aboutUs.sarka.title")}</h2>
-          <span>
-            <a href="https://www.linkedin.com/in/sarka-flidrova">LinkedIn</a> |{" "}
-            <a href="https://github.com/suffle-girl">GitHub</a>
-          </span>
-          <h3 className="about-us--title--name__sarka">
-            {t("aboutUs.sarka.subtitle")}
-          </h3>
-          <p className="about-us--title--paragraph">
-            {t("aboutUs.sarka.description")}
-          </p>
-          <img
-            className="about-us--title--img about-us--title--img__sarka"
-            src={sarka}
-            alt="sarka avatar"
-          />
-        </div>
+      <div className="about-us--title about-us--title--sarka">
+        <h2 className="about-us--item about-us--title--name">
+          {t("aboutUs.sarka.title")}
+        </h2>
+        <span className="about-us--item">
+          <a
+            className="about-us--link"
+            href="https://www.linkedin.com/in/sarka-flidrova"
+          >
+            LinkedIn
+          </a>{" "}
+          |{" "}
+          <a className="about-us--link" href="https://github.com/suffle-girl">
+            GitHub
+          </a>
+        </span>
+        <h3 className="about-us--item about-us--title--name__sarka">
+          {t("aboutUs.sarka.subtitle")}
+        </h3>
+        <p className="about-us--item about-us--title--paragraph">
+          {t("aboutUs.sarka.description")}
+        </p>
+        <img
+          className="about-us--item about-us--title--img about-us--title--img__sarka"
+          src={sarka}
+          alt="sarka avatar"
+        />
       </div>
     </div>
   );
