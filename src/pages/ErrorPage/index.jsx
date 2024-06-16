e4cd747e588b850cecb04f1990262ba104ec0733
Šárka Flídrová <sarka.flidrova@email.cz>
2024-06-16 13:28:39 +0200

    com. ErrorPage: přidány vlastní styly a upraveny obrázky

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -12,29 +12,27 @@ export const ErrorPage = () => {
   return (
     <Suspense fallback="loading">
       <Layout>
-        <div className="container__quiz">
-          <h1 className="quiz-logo">MoodAdviser</h1>
+        <div className="container__error">
+          <h1 className="error-logo">MoodAdviser</h1>
           <h2 className="error-message">{t("errorMessage")}</h2>
-          <div className="quiz__lastSentence">
-            <div className="quiz__lastSentence--recommended">
-              {t("quiz.last")}
-            </div>
-            <div className="quiz__lastSentence-menu">
-              <a className="quiz__lastSentence-menu-link" href="/breath">
+          <div className="error__content">
+            <div className="error__content--recommended">{t("quiz.last")}</div>
+            <div className="error__content-menu">
+              <a className="error__content-menu-link" href="/breath">
                 <img src={breath} alt="breath" />
-                <p className="quiz__lastSentence-menu--link-text">
+                <p className="error__content-menu--link-text">
                   {t("navigation.breath")}
                 </p>
               </a>
-              <a className="quiz__lastSentence-menu--link" href="/music">
+              <a className="error__content-menu--link" href="/music">
                 <img src={music} alt="music" />
-                <p className="quiz__lastSentence-menu--link-text">
+                <p className="error__content-menu--link-text">
                   {t("navigation.music")}
                 </p>
               </a>
-              <a className="quiz__lastSentence-menu--link" href="/exercises">
+              <a className="error__content-menu--link" href="/exercises">
                 <img src={exercie} alt="exercises" />
-                <p className="quiz__lastSentence-menu--link-text">
+                <p className="error__content-menu--link-text">
                   {t("navigation.exercises")}
                 </p>
               </a>
